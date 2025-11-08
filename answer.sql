643d2b09ae9c159382d81f719cd6d99344d0584b
Abdullah Alturki <abdullahoalturki@gmail.com>
2025-11-08 13:25:45 +0300

    -

--- answer.sql
+++ answer.sql
@@ -1,1 +1,17 @@
 -- ADD YOUR SQL QUERY HERE
+
+SELECT 
+  it.investor_id,
+  s.sector_name,
+  ROUND(
+    (it.no_of_shares::NUMERIC / t.total_shares) * 100,
+    2
+  ) AS share_percentage
+FROM investor_transactions it
+JOIN sectors s ON it.sector_id = s.sector_id
+JOIN (
+  SELECT investor_id, SUM(no_of_shares) AS total_shares
+  FROM investor_transactions
+  GROUP BY investor_id
+) t ON it.investor_id = t.investor_id
+ORDER BY it.investor_id, share_percentage DESC;
